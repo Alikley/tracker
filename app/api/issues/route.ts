f75055fe607e08506f9d17ac076611537520a9e1
Alikley <alikley933@gmail.com>
2025-02-27 01:09:37 +0330

    Build an api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+const schema = z.object({
+  title: z.string().min(1).max(255),
+  decription: z.string().min(1),
+});
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, decription: body.decription },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
