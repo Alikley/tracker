827936d97a23b7f6e9859dd24edc0aca511b9952
Alikley <alikley933@gmail.com>
2025-03-13 12:33:04 +0330

    Sorting Issue

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,37 +1,46 @@
-"use client";
-import { Status } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
-import React from "react";
+'use client';
 
-const statuses: { label: string; value: string }[] = [
-  { label: "ALL", value: "ALL" }, // Now has explicit value
-  { label: "Open", value: "OPEN" },
-  { label: "In Progress", value: "IN_PROGRESS" },
-  { label: "Close", value: "CLOSED" },
+import { Status } from '@prisma/client';
+import { Select } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react';
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: 'All' },
+  { label: 'Open', value: 'OPEN' },
+  { label: 'In Progress', value: 'IN_PROGRESS' },
+  { label: 'Closed', value: 'CLOSED' },
 ];
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
-    <div>
-      <Select.Root
-        onValueChange={(status) => {
-          const query = status ? `?status=${status}` : "";
-          router.push("/issues/list" + query);
-        }}
-      >
-        <Select.Trigger placeholder="Filter by status..." />
-        <Select.Content>
-          {statuses.map((status) => (
-            <Select.Item key={status.value} value={status.value}>
-              {status.label}
-            </Select.Item>
-          ))}
-        </Select.Content>
-      </Select.Root>
-    </div>
+    <Select.Root
+      defaultValue={searchParams.get('status') || ''}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append('status', status);
+        if (searchParams.get('orderBy'))
+          params.append('orderBy', searchParams.get('orderBy')!);
+
+        const query = params.size ? '?' + params.toString() : '';
+        router.push('/issues/list' + query);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item
+            key={status.value}
+            value={status.value}
+          >
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
   );
 };
 
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,19 +2,40 @@ import { Table } from "@radix-ui/themes";
 import { prisma } from "@/prisma/client";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueAction from "./IssueAction";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 interface Props {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }
 const IssuesPage = async ({ searchParams }: Props) => {
+  const columms: { label: string; value: keyof Issue; classNames?: string }[] =
+    [
+      { label: "Issue", value: "title" },
+      { label: "Status", value: "status", classNames: "hidden md:table-cell" },
+      {
+        label: "Created",
+        value: "createdAt",
+        classNames: "hidden md:table-cell",
+      },
+    ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columms
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   return (
     <div>
@@ -22,13 +43,23 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columms.map((column) => (
+              <Table.ColumnHeaderCell key={column.value}>
+                <NextLink
+                  href={{
+                    query: {
+                      status: searchParams.status,
+                      orderBy: column.value,
+                    },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
